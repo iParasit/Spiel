bcb7f05d0fbf20345957c84eb54097342bac1cbc
iParasit <dennis.schmidt19850809@outlook.de>
2022-02-19 20:12:26 +0100

    kommentare hinzugefügt

--- src/de/test/de/Spiel.java
+++ src/de/test/de/Spiel.java
@@ -1,17 +1,21 @@
 package de.test.de;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.ThreadLocalRandom;
 
-    // D A S    S P I E L    W I R D     G E N E R I E R T
+
+// D A S    S P I E L    W I R D     G E N E R I E R T
 
 public class Spiel {
     static Integer count = 0;
     static Integer myNumber = ThreadLocalRandom.current().nextInt(0, 100 + 1);
     static JLabel text = new JLabel("Gebe eine Zahl zwischen 0 - 100 ein");
     static JTextField textField = new JTextField();
-    static JLabel countText = new JLabel("Anzahl der Versuche: " + String.valueOf(count));
+    static JLabel countText = new JLabel();
+    static JButton button = new JButton("Abschicken");
+    static JFrame frame = new JFrame("Rate die Zahl!");
 
     public static void main(String[] args) {
         openUi();
@@ -22,7 +26,7 @@ public class Spiel {
 
 
     public static void openUi() {
-        JFrame frame = new JFrame("Rate die Zahl!");
+
         frame.setSize(400, 400);
         frame.setLocation(100, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,7 +38,7 @@ public class Spiel {
         text.setBounds(50, 50, 500, 30);
         textField.setBounds(50, 100, 200, 30);
 
-        JButton button = new JButton("Abschicken");
+        button.setBackground(Color.lightGray);
         button.setBounds(50, 150, 200, 30);
 
         button.addActionListener(new ActionListener() {
@@ -60,12 +64,14 @@ public class Spiel {
         frame.setVisible(true);
 }
 
+
     // T E X T F E L D
 
 
     public static void guess(Integer number) {
         if (number.equals(myNumber)) {
             text.setText("Richtig geraten!");
+
         } else {
             count ++;
             if(number < myNumber) {
